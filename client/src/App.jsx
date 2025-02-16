26e8b5ba22ccecd946dc19122fb5b1bfca0099cd
Paul Philips <paulphilips@email.com>
2025-02-16 17:27:17 +0530

    Initialized project with folder structure

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-yellow-600 text-6xl underline uppercase cursor-pointer">
+        vishal prajapati
+      </h1>
+    </div>
+  );
+}
+
+export default App;
